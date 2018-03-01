f39017d6ece6b652fdd0acd759a74856b12d0422
Rand0mUsers <Rand0mUsers@users.noreply.github.com>
2018-03-01 19:06:23 +0000

    Minified

--- hydroxyl-acid/interactivity.js
+++ hydroxyl-acid/interactivity.js
@@ -1,17 +1,1 @@
-"use strict";
-
-$(document).ready(function () {
-    // click handler for dropdowns
-    $(".toggle").on("click", function() {
-        // slide-toggle the content
-        $(this).next().slideToggle(200);
-
-        // toggle the little triangle appropriately
-        if ($(this)[0].innerHTML.startsWith("▸")) {
-            $(this)[0].innerHTML = $(this)[0].innerHTML.replace("▸", "▾");
-        }
-        else {
-            $(this)[0].innerHTML = $(this)[0].innerHTML.replace("▾", "▸");
-        }
-    });
-});
+"use strict";$(document).ready(function(){$(".toggle").on("click",function(){$(this).next().slideToggle(200),$(this)[0].innerHTML.startsWith("▸")?$(this)[0].innerHTML=$(this)[0].innerHTML.replace("▸","▾"):$(this)[0].innerHTML=$(this)[0].innerHTML.replace("▾","▸")})});
