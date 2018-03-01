7d3680e6b8041787330ab934446c4ce4dd7d9eb2
Rand0mUsers <Rand0mUsers@users.noreply.github.com>
2018-03-01 17:50:52 +0000

    Last pics & enhancements

--- interactivity.js
+++ interactivity.js
@@ -1,6 +1,17 @@
+"use strict";
+
 $(document).ready(function () {
-    console.log("Ready!");
+    // click handler for dropdowns
     $(".toggle").on("click", function() {
+        // slide-toggle the content
         $(this).next().slideToggle(1000);
+
+        // toggle the little triangle appropriately
+        if ($(this)[0].innerHTML.startsWith("▸")) {
+            $(this)[0].innerHTML = $(this)[0].innerHTML.replace("▸", "▾");
+        }
+        else {
+            $(this)[0].innerHTML = $(this)[0].innerHTML.replace("▾", "▸");
+        }
     });
 });
