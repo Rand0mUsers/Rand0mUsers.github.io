72b1230e11a71ff32313d5b39c9daae2b887c0d7
Rand0mUsers <Rand0mUsers@users.noreply.github.com>
2018-03-04 16:30:45 +0000

    Fixed dropdown arrows on IE/pre-ECMAScript 2015

--- hydroxyl-acid/interactivity.js
+++ hydroxyl-acid/interactivity.js
@@ -1,1 +1,1 @@
-"use strict";$(document).ready(function(){$(".toggle").on("click",function(){$(this).next().slideToggle(200),$(this)[0].innerHTML.startsWith("▸")?$(this)[0].innerHTML=$(this)[0].innerHTML.replace("▸","▾"):$(this)[0].innerHTML=$(this)[0].innerHTML.replace("▾","▸")})});
+"use strict";$(document).ready(function(){$(".toggle").on("click",function(){$(this).next().slideToggle(200),"▸"===$(this)[0].innerHTML[0]?$(this)[0].innerHTML=$(this)[0].innerHTML.replace("▸","▾"):$(this)[0].innerHTML=$(this)[0].innerHTML.replace("▾","▸")})});
